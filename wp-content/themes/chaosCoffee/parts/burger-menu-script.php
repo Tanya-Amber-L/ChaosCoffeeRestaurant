1e34d18c8188eb94dd6dbf9700077a9625485113
Tanya Leenders <tanya.leenders@gmail.com>
2021-01-20 16:04:16 +0100

    burger menu okay

--- wp-content/themes/chaosCoffee/parts/burger-menu-script.php
+++ wp-content/themes/chaosCoffee/parts/burger-menu-script.php
@@ -1,10 +1,10 @@
 <script>
-function myFunction() {
-  var menu = document.querySelector(".menu-nav");
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
+  function showMenu() {
+    const menu = document.querySelector(".menu-nav");
+    if (menu.style.display === "flex") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
   }
-}
 </script>
