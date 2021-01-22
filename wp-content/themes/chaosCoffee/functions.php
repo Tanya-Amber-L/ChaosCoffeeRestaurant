1fff90c388e534d62c1a6f3d75a5f76397a6b257
Bertrand2 <dossogne.bertrand@gmail.com>
2021-01-22 09:31:04 +0100

    archive recipes

--- wp-content/themes/chaosCoffee/functions.php
+++ wp-content/themes/chaosCoffee/functions.php
@@ -3,6 +3,7 @@
 function theme_register_assets () {
 	//wp_register_style( 'themeStyle', 'localhost/testWordpress/wp-content/themes/testTheme/style.css' );
   wp_enqueue_style( 'main-style', get_template_directory_uri() . "/style.css");
+  wp_enqueue_style( 'home-style', get_template_directory_uri() . "/style/home.css", ['main-style']);
   wp_enqueue_style( 'recipe-style', get_template_directory_uri() . "/style/recipe.css", ['main-style']);
   wp_enqueue_style( 'slider-style', get_template_directory_uri() . "/slider.css", ['main-style']);
   wp_enqueue_script( 'slider-script', get_template_directory_uri() . '/js/slider-script.js' );
@@ -110,3 +111,10 @@ function wpb_load_widget() {
     register_widget( 'chaos_coffee_widget' );
 }
 add_action( 'widgets_init', 'wpb_load_widget' );
+
+
+//remove Gutenberg
+function disable_gutenberg_editor() {
+    return false;
+}
+add_filter("use_block_editor_for_post_type", "disable_gutenberg_editor");
